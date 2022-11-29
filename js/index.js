3e5318c674b36abc2c563aff229f4c17969dc582
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-29 17:09:04 +0100

    add remove tasks & add else

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ createApp({
             tasksInput : 0,
             logo: 'img/IKEA-logo.png',
             newTask:'',
+            error: false,
             // All Data
             tasks : 
             [
@@ -21,9 +22,6 @@ createApp({
                     text: 'Fare il bucato', 
                     done: false 
                 },
-                {
-                    text: 'ciao'
-                }
             ]
 
         }
@@ -31,9 +29,19 @@ createApp({
     methods:{
         // metodi
         addTask(){
-            // Prima di tutto mi creo l'object e inserisco al suo interno il tag text perché poi verrà utilizzato per cercare la srtinga di testo. Successivamente inserisco this.newTask che è collegato all'input
-            const obj = {text: `${this.newTask}`};
-            this.tasks.push(obj);
+            // Prima di tutto mi creo l'object e inserisco al suo interno il tag text perché poi verrà utilizzato per cercare la srtinga di testo. Successivamente inserisco this.newTask che è collegato all'input.
+            if (this.newTask.length < 5 || this.newTask === '') {
+                this.error=true;
+            }else{
+                this.error=false;
+                const obj = {text: `${this.newTask}`};
+                this.tasks.push(obj);
+                // Pulisco l'input dopo l'invio, e se esce errore resta ciò che ho scritto.
+                this.newTask = '';
+            }
+        },
+        deletTask(index){
+            this.tasks.splice(index, 1);
         }
     }
 
